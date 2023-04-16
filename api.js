bfade460519bd349dc7a858afef1fe7b5917f08b
Rakhymzhan <mark.john.99@mail.ru>
2023-04-16 01:35:53 -0700

    improvements

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 // Замени на свой, чтобы получить независимый от других набор данных.
 // "боевая" версия инстапро лежит в ключе prod
-const personalKey = "prod";
+const personalKey = "Reha";
 const baseHost = "https://webdev-hw-api.vercel.app";
 const postsHost = `${baseHost}/api/v1/${personalKey}/instapro`;
 
@@ -22,6 +22,44 @@ export function getPosts({ token }) {
       return data.posts;
     });
 }
+
+export function   getUserPosts({ token, id }) {
+  return fetch(postsHost + '/user-posts/' + id, {
+    method: "GET",
+    headers: {
+      Authorization: token,
+    },
+  })
+    .then((response) => {
+      if (response.status === 400) {
+        throw new Error("Нет авторизации");
+      }
+      return response.json();
+    })
+      .then((data) => {
+        return data.posts;
+      });
+}
+
+
+export function onAddPostClick({ description, imageUrl, token }) {
+  return fetch(postsHost, {
+    method: "POST",
+    headers: {
+      Authorization: token,
+    },
+    body: JSON.stringify({
+      "description": description,
+      "imageUrl": imageUrl,
+    }),
+  }).then((response) => {
+    if (response.status === 400) {
+      throw new Error("Some Error!")
+    }
+    return response.json();
+  });
+}
+
 
 // https://github.com/GlebkaF/webdev-hw-api/blob/main/pages/api/user/README.md#%D0%B0%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D1%8C%D1%81%D1%8F
 export function registerUser({ login, password, name, imageUrl }) {
@@ -67,4 +105,22 @@ export function uploadImage({ file }) {
   }).then((response) => {
     return response.json();
   });
+}
+
+
+export function fetchLike({ token, postId, isLiked }) {
+  {
+    return fetch(postsHost + `/${postId}${isLiked ? '/dislike' : '/like'}`, {
+      method: "POST",
+      headers: {
+        Authorization: token,
+      },
+    })
+      .then((response) => {
+        if (response.status === 400) {
+          throw new Error("Some Error!");
+        }
+        return response.json();
+      });
+  }
 }
